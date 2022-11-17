9bb7e081c2b9aec0e717e0b8d8f02c3c7702d47f
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2022-11-17 16:03:36 +0700

    update remove.c

--- NachOS-4.0/code/test/remove.c
+++ NachOS-4.0/code/test/remove.c
@@ -5,6 +5,10 @@ int main()
     PrintString("Nhap ten file can xoa: ");
     char *name;
     ReadString(name, 255);
-    Remove(name);
+    int res = Remove(name);
+    if (res == -1)
+        PrintString("Cannot remove file!!!\n");
+    else
+        PrintString("Remove successfully!\n");
     Halt();
 }
